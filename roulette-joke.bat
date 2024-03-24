f19eaa32fb34d5f962705800a9fe8d01f1068d4b
Noah Bruijninckx <66059559+DiamsLabs543@users.noreply.github.com>
2024-03-24 02:48:39 +0100

    Create roulette-joke.bat

---
+++ roulette-joke.bat
@@ -0,0 +1,42 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Effacer l'écran
+cls
+
+REM Configuration
+set "chances=6"
+
+:game_loop
+REM Affichage de l'interface
+echo.
+echo ####################################
+echo #                                  #
+echo #            BIENVENUE             #
+echo #                                  #
+echo #   Voici le jeu de la roulette    #
+echo #                                  #
+echo #          1 chance sur 6          #
+echo #            de mourir !           #
+echo #                                  #
+echo ####################################
+echo.
+
+REM Attendre l'appui sur une touche
+pause >nul
+
+REM Tirage au sort
+set /a "bullet=!random! %% chances"
+
+REM Affichage du résultat
+echo.
+set "result=Tu as survecu ! (O_o)"
+if %bullet% equ 0 set "result=Tu es mort ! (X_x)"
+echo %result%
+
+REM Rejouer
+goto :game_loop
+
+REM Fin du jeu
+echo Merci d'avoir joue !
+pause >nul
